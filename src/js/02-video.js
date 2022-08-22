72e89c72ac4093547c889bf51e0a016b81f164cc
Igor Mazur <62945459+Grungeshum@users.noreply.github.com>
2022-08-22 19:07:57 +0300

    fix task#2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,38 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on(
-  'timeupdate',
-  throttle(evt => {
-    localStorage.setItem('videoplayer-current-time', evt.seconds);
-  }, 1000)
-);
+player.on('play', function () {
+  let seconds = localStorage.getItem('videoplayer-current-time');
+  seconds = parseFloat(seconds);
+  if (!seconds) {
+    seconds = 0;
+  }
+  player
+    .setCurrentTime(seconds)
+    .then(function (seconds) {
+      console.log('Time has set to:' + seconds);
+      player.off('play');
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          console.error(
+            "the time was less than 0 or greater than the video's duration"
+          );
+          break;
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+        default:
+          console.error('Vimeo player:error occurred', error);
+          break;
+      }
+    });
+});
+
+function onTimeUpdate(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
